2e888d533a9887d5aaffbc7ff03ab813726a6c20
Omar Diab <42656647+omardballin@users.noreply.github.com>
2019-02-15 15:45:59 -0600

    Add files via upload

---
+++ Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dog
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dog puppy = new Dog("Orion", "Shawn", 1, Gender.Male);
+            Console.WriteLine(puppy.Bark(3));
+            Console.WriteLine(puppy.GetTag());
+
+            Dog myDog = new Dog("Lileu", "Dale", 4, Gender.Female);
+            Console.WriteLine(myDog.Bark(1));
+            Console.WriteLine(myDog.GetTag());
+
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+    }
+}
+
