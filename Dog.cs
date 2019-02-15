bc52fb30c514d7821bab657c122c30dfef1cff38
Omar Diab <42656647+omardballin@users.noreply.github.com>
2019-02-15 15:47:32 -0600

    Update Dog.cs

--- Dog.cs
+++ Dog.cs
@@ -1,4 +1,57 @@
-﻿using System;
+namespace dog
+{
+    public enum Gender { Male, Female };
+
+    public class Dog
+    {
+        public string name;
+        public string owner;
+        int age;
+        public Gender gender;
+
+        public Dog(string Name, string Owner, int Age, Gender Gender)
+        {
+            name = Name;
+            owner = Owner;
+            age = Age;
+            gender = Gender;
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("Woof");
+        }
+
+        public string Bark(int times)
+        {
+            string strBarks = "Woof";
+
+            if (times <= 0)
+            {
+                return "";
+            }
+
+            for (int i = 1; i < times; i++)
+            {
+                strBarks += " Woof";
+            }
+
+            return strBarks;
+        }
+
+        public string GetTag()
+        {
+            string strGender1 = gender == Gender.Male ? "His" : "Her";
+            string strGender2 = gender == Gender.Male ? "he" : "she";
+            string strAgeText = age == 1 ? " year old" : " years old";
+             
+            return "If lost, call " + owner + ". " + strGender1 + " name is " + name + " and " + strGender2 + " is " + age + strAgeText +".";
+
+        }
+    }
+}
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
